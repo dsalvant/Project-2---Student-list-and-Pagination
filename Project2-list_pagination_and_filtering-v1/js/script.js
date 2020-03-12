d874cae604491b9be7c6243b07e8d5ce42f8b28c
dsalvant <37490462+dsalvant@users.noreply.github.com>
2020-03-12 13:05:26 -0400

    Now the pageLink work when click
    now when clicking the page link it works

--- Project2-list_pagination_and_filtering-v1/js/script.js
+++ Project2-list_pagination_and_filtering-v1/js/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
    const showPage = (list, page) => {
       list = listOfStudents.children;
       // adding constant for the start and ending point
-      const startPoint = (tenStudentPerPage * page) - tenStudentPerPage;
+      const startPoint = (tenStudentPerPage * page) - 9;
       const endPoint = tenStudentPerPage * page;
       //numberOfPage = list / tenStudentPerPage;
       //console.log(list);
@@ -48,11 +48,15 @@ document.addEventListener('DOMContentLoaded', (e) => {
    I need to create the second function name appendPageLinks to activate the links for page numbering at the bottom of the page. 
    */
    const appendPageLinks = (list) => {
+      
       const div = document.createElement('div');
       div.className = 'pagination';
       const ul = document.createElement('ul');
       div.appendChild(ul);
+      
+      
       const numberOfPage = Math.ceil(list.length / tenStudentPerPage);
+      
       for (let i = 0; i < numberOfPage; i++) {
          const li = document.createElement('li');
          const a = document.createElement('a');
@@ -63,22 +67,84 @@ document.addEventListener('DOMContentLoaded', (e) => {
          a.href = '#';
          a.textContent = i+1;
 
-         if (i === 0) {
-            a.className = 'active';
-         }
+         
+         
+         //a.className = 'active';
+         //a.className = '';
+      
+         
+
+         
          /*
          Add the active class name to the first pagination link initially.
          add an eventlistener on the page link when click at the bottom of the page 
          */
          a.addEventListener('click', (e) => {
-            const paginationLink = document.getElementsByTagName('A');
-            //console.log(a);
+            
+            const paginationLink = document.getElementsByTagName('A'); 
+            //a.className = 'active';
+            
             for (let i = 0; i < paginationLink.length; i++) {
                paginationLink[i].className = '';
-
-               //this activate the classList
-               e.target.paginationLink = 'active'; 
+               e.target.paginationLink = 'active';
             }
+            
+            if (i === 0) {
+            
+               a.className = 'active';
+               for (let i = 0; i < paginationLink.length; i++)
+               paginationLink[i].className = '';
+            } else {
+               paginationLink[i].className = 'active';
+            }
+            
+            
+            // for (let i = 0; i > a.length; i ++) {
+            //    if (i === 0) {
+            //       a.className = 'active';
+            //    }
+            // }
+            // a.className = '';
+            // for (let i = 0; i > a.length; i++) {
+            //    paginationLink[i].className = '';
+            //    if (i === 0) {
+            //       a.className = 'active';
+            //    } else {
+            //       paginationLink[i].className = 'active';
+            //    }
+            //    // else {
+            //    //    paginationLink[i].className = 'active';
+            //    // }
+               
+            // }     
+            // for (let i = 0; i < paginationLink.length; i++) {
+               
+
+            //    if (i === 0) {
+            //       a.className = 'active';  
+            //    } else {
+            //       paginationLink[i].className = '';
+            //    }
+            //    if (i === 0) {
+            //       a.className = 'active';
+            //    } else {
+            //       paginationLink[i].className = 'active';
+            //    }
+            //    // } else {
+                  
+            //    //    a.className = '';  
+            //    // } else {
+            //    // //this activate the classList
+            //    // //e.target.paginationLink = 'active'; 
+            //    // a.className = 'active';  
+            //    // }
+            //    // }
+            //    //e.target.paginationLink = 'active'; 
+              
+            //    // console.log(page);
+            //    // console.log(paginationLink);
+            //    console.log(a);
+            // }
             showPage(list, e.target.textContent);
          });
       }
